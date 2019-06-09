09c06ebaf0ca4e79fdd43d9c2bb82bd62be19bb3
giordano200699 <giordano200699@gmail.com>
2019-06-08 21:36:47 -0500

    Demanda Inversa

--- application/controllers/ApiController.php
+++ application/controllers/ApiController.php
@@ -167,6 +167,14 @@ class ApiController extends REST_Controller {
                 echo("Faltan algunos de los datos de la fecha_inicio o fecha_fin");
             }
         }
+        public function demandaInversa_get(){
+            if($this->get("fecha_inicio")&& $this->get("fecha_fin")){
+                $array_out = $this->pago->listarDemandaInversa($this->get("fecha_inicio"),$this->get("fecha_fin"));
+                echo json_encode($array_out);
+            }else{
+                echo("Faltan algunos de los datos de la fecha_inicio o fecha_fin");
+            }
+        }
 
         public function cantidadPorPeriodoAnio_get(){
             $yearStart = $this->get("year_inicio");
